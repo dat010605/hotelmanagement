65a47b2538ce3ad1841f05ee95406e647be59f8e
Nguyễn Văn Hoài <13.nguyenvanhoai.11a29@gmail.com>
2026-05-23 01:43:10 +0700

    fix: cap nhat code hien thi anh, sua footer va backup database kem data anh

--- obj/Debug/net10.0/HotelManagement.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/HotelManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c6615533aee1f2ccccb56201de066c55a8e9d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98235662d39a581dfbd12c1ed74d396bde5ec8a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
